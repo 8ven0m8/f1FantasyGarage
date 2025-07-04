df8cca32d47e936b86af2a76ea9f874fe9f056c1
Pranjal <sapkotapranjal25@gmail.com>
2025-07-05 01:45:45 +0545

    tailwind config

--- root/root/settings.py
+++ root/root/settings.py
@@ -155,6 +155,11 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# For Railway deployment
+if not DEBUG:
+    # Collect static files for production
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
