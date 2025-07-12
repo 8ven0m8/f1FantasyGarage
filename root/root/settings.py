bcc39c70d4543fb2473c0dd90b3cf6dc4236bed1
Pranjal <sapkotapranjal25@gmail.com>
2025-07-12 16:28:24 +0530

    3

--- root/root/settings.py
+++ root/root/settings.py
@@ -35,8 +35,8 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'f1fantasygarage.up.railway.app', 'localhost', '0.0.0.0', '*']
-CSRF_TRUSTED_ORIGINS = ['https://f1fantasygarage.up.railway.app/',]
+ALLOWED_HOSTS = ['127.0.0.1', 'f1fantasygarage-production.up.railway.app', 'localhost', '0.0.0.0']
+CSRF_TRUSTED_ORIGINS = ['https://f1fantasygarage-production.up.railway.app',]
 
 
 # Application definition
