fe9861f799878a9c9c610560a27752d3f2f1c3b4
Pranjal <sapkotapranjal25@gmail.com>
2025-07-16 01:34:55 +0530

    added debug True

--- root/root/settings.py
+++ root/root/settings.py
@@ -35,6 +35,8 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
+DEBUG = True
+
 ALLOWED_HOSTS = ['127.0.0.1', 'f1fantasygarage-production.up.railway.app', 'localhost', '0.0.0.0', '*']
 CSRF_TRUSTED_ORIGINS = ['https://f1fantasygarage-production.up.railway.app']
 INTERNAL_IPS = ['127.0.0.1', '*']
