2d59cb0255b75b615e7523b3aa375cabcae33ef3
Pranjal <sapkotapranjal25@gmail.com>
2025-07-17 10:59:40 +0530

    Cloudinary implementation

--- root/garage/models.py
+++ root/garage/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class TopLeaderboard(models.Model):
     name = models.CharField(max_length=40)
     team = models.CharField(max_length=40)
     points = models.IntegerField()
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    # image = models.ImageField(upload_to='images/', blank=True, null=True) # for local databases
+    image = CloudinaryField('image', blank=True, null=True)
 
     def __str__(self):
         return self.name
